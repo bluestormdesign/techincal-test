5fd17e9c8bd814e60b704685efc03ac33517eb40
Denis Mekina <denis.mekina@bluestormdesign.co.uk>
2019-07-03 14:20:27 +0100

    Search fixed

--- src/javascript/matrix.js
+++ src/javascript/matrix.js
@@ -31,14 +31,26 @@ $(function () {
 		]
 	});
 
-	$('.gallery-grid, .gallery-slider, .gallery-pinterest').magnificPopup({
+	// Image popups
+	$('.gallery-pinterest, .gallery-grid, .gallery-slider').magnificPopup({
 		delegate: 'a',
 		type: 'image',
+		removalDelay: 500, //delay removal by X to allow out-animation
 		gallery: {
 			enabled: true,
 			navigateByImgClick: true,
 		},
+		callbacks: {
+			beforeOpen: function() {
+				// just a hack that adds mfp-anim class to markup
+				this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+				this.st.mainClass = this.st.el.attr('data-effect');
+			}
+		},
+		closeOnContentClick: true,
+		midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
 	});
+
 
 	$('.gallery-slider-top').slick({
 		slidesToShow: 1,
