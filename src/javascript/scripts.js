b3064f7c64805ea0b7f0a0dfc2acd62cbd36e92a
Denis Mekina <denis.mekina@bluestormdesign.co.uk>
2019-07-09 15:59:55 +0100

    Text size functionality build

--- src/javascript/scripts.js
+++ src/javascript/scripts.js
@@ -59,4 +59,65 @@ $(function () {
 		Cookies.remove('contrast');
 	});
 
+	function changeFontSize(option, tag, value) {
+		var element = $(tag);
+		if (element) {
+			var fontSize = element.css('font-size').slice(0, -2);
+			if (option === 'increase') {
+				var updatedFontSize = parseInt(fontSize) + value + 'px';
+			} else if (option === 'decrease') {
+				var updatedFontSize = parseInt(fontSize) - value + 'px';
+			}
+			element.css('font-size', updatedFontSize);
+		}
+	}
+
+	function changeFontSizes(option, tags, size) {
+		for (var i = 0; i < tags.length; i++) {
+			changeFontSize(option, tags[i], size)
+		}
+	}
+
+	function loadFontSize() {
+		if(Cookies.get("text-size") === 'small') {
+			changeFontSizes('decrease', ['body', 'h1', 'p', 'a.small', 'a.medium', 'a.large', '#overlay-menu li a', '#info li a', '#contact-details p', '#breadcrumb li'], 5);
+		} else if(Cookies.get("text-size") === 'large') {
+			changeFontSizes('increase', ['body', 'h1', 'p', 'a.small', 'a.medium', 'a.large', '#overlay-menu li a', '#info li a', '#contact-details p', '#breadcrumb li'], 5);
+		}
+	}
+	loadFontSize();
+
+	$('a.large').click( function() {
+		if(Cookies.get("text-size") === 'large') {
+			return;
+		}
+		changeFontSizes('increase', ['body', 'h1', 'p', 'a.small', 'a.medium', 'a.large', '#overlay-menu li a', '#info li a', '#contact-details p', '#breadcrumb li'], Cookies.get("text-size") === 'small' ? 10 : 5);
+
+		if(Cookies.get("text-size") === undefined){
+			Cookies.set('text-size', 'large');
+		} else if(Cookies.get("text-size") === 'small') {
+			Cookies.remove('text-size');
+			Cookies.set('text-size', 'large');
+		}
+	});
+
+	$('a.small').click( function() {
+		if(Cookies.get("text-size") === 'small') {
+			return;
+		}
+		changeFontSizes('decrease', ['body', 'h1', 'p', 'a.small', 'a.medium', 'a.large', '#overlay-menu li a', '#info li a', '#contact-details p', '#breadcrumb li'], Cookies.get("text-size") === 'large' ? 10 : 5);
+
+		if(Cookies.get("text-size") === undefined){
+			Cookies.set('text-size', 'small');
+		} else if(Cookies.get("text-size") === 'large') {
+			Cookies.remove('text-size');
+			Cookies.set('text-size', 'small');
+		}
+	});
+
+	$('a.medium').click( function() {
+		Cookies.remove('text-size');
+		location.reload();
+	});
+
 });
